4cab15fae73e9578c43bb652b1b67815d6a33935
themmixproject <steven.devalk@gmail.com>
2024-12-27 20:28:08 +0100

    Added SectionHeaderLink and dependencies: Added Anchor datastructure to store both the label and the href. This datastructure has been added in order to prevent both the label and the href to be added as properties to components; Added and implemented SectionHeaderLink component; Added VerticalCenterWrapper component to easily vertically center elements when needed;

---
+++ NewTube/NewTube.Client/Models/Anchor.cs
@@ -0,0 +1,14 @@
+﻿namespace NewTube.Client.Models
+{
+    public class Anchor
+    {
+        public string? Href { get; set; }
+        public string? Label { get; set; }
+    
+        public Anchor(string href, string label)
+        {
+            Href = href;
+            Label = label;
+        }
+    }
+}
