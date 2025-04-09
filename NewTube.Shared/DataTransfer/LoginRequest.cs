6660f17875a0160fc85b6d77c467f1d6b9c78ef0
themmixproject <steven.devalk@gmail.com>
2025-04-09 13:38:43 +0200

    Renamed UserName property to Username in NewTube.Shared.DataTransfer.LoginRequest;

--- NewTube.Shared/DataTransfer/LoginRequest.cs
+++ NewTube.Shared/DataTransfer/LoginRequest.cs
@@ -4,6 +4,6 @@ namespace NewTube.Shared.DataTransfer;
 
 public class LoginRequest
 {
-    public string UserName { get; set;}
+    public string Username { get; set;}
     public string Password { get; set; }
 }
