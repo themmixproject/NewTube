f7f16f46d8bb325fbbe8b9870eb71e4efc49e8bd
themmixproject <steven.devalk@gmail.com>
2025-04-09 14:10:17 +0200

    Added empty-parameter constructor to LoginRequest;

--- NewTube.Shared/DataTransfer/LoginRequest.cs
+++ NewTube.Shared/DataTransfer/LoginRequest.cs
@@ -4,6 +4,8 @@ namespace NewTube.Shared.DataTransfer;
 
 public class LoginRequest
 {
+    public LoginRequest() { }
+
     public string Username { get; set;}
     public string Password { get; set; }
 }
