f0d082d255cc1011ad9c7d297d5b6b8ae2114d5a
themmixproject <steven.devalk@gmail.com>
2025-04-10 15:09:09 +0200

    Added base for AuthClient client in NewTube.Client;

---
+++ NewTube/NewTube.Client/Clients/AuthClient.cs
@@ -0,0 +1,13 @@
+﻿namespace NewTube.Client.Clients
+{
+    public class AuthClient
+    {
+        private string baseUrl = "auth/";
+        private HttpClient httpClient;
+        
+        public AuthClient(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+    }
+}
