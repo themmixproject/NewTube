722e6067b124badef5805a26e7884e5cff76f630
themmixproject <steven.devalk@gmail.com>
2025-04-10 14:09:11 +0200

    Added HttpClient singleton in NewTube.Client;

--- NewTube/NewTube.Client/Program.cs
+++ NewTube/NewTube.Client/Program.cs
@@ -13,6 +13,10 @@ namespace NewTube.Client
             builder.Services.AddAuthorizationCore();
             builder.Services.AddCascadingAuthenticationState();
             builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             await builder.Build().RunAsync();
         }
