88a91fb8fd212df0a665434b3ce899d3f7d6006a
themmixproject <steven.devalk@gmail.com>
2025-04-10 15:23:41 +0200

    Added AuthClient scoped service in Program;

--- NewTube/NewTube.Client/Program.cs
+++ NewTube/NewTube.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NewTube.Client;
+using NewTube.Client.Clients;
 
 namespace NewTube.Client
 {
@@ -17,6 +18,7 @@ namespace NewTube.Client
             {
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
             });
+            builder.Services.AddScoped<AuthClient>();
 
             await builder.Build().RunAsync();
         }
