5bcada10beb178bcf59a82df3d78b0a5e68e2767
themmixproject <steven.devalk@gmail.com>
2025-04-10 13:54:45 +0200

    Added base for AuthController class;

---
+++ NewTube/NewTube.Server/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewTube.Server.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController
+    {
+        public AuthController() { }
+
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "Hello World!";
+        }
+    }
+}
