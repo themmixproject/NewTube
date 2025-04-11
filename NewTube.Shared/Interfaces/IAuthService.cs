8d1401aab943f27f145354db62c06eec81b159ef
themmixproject <steven.devalk@gmail.com>
2025-04-11 15:33:37 +0200

    Added base for IAuthService to NewTube.Shared.Interfaces;

---
+++ NewTube.Shared/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using NewTube.Shared.DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTube.Shared.Interfaces
+{
+    internal class IAuthService
+    {
+        public void RequestLogin(LoginRequest loginRequest);
+    }
+}
