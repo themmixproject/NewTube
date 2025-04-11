24c9880ad6d96b346a0c4dbbf5c374754a44d488
themmixproject <steven.devalk@gmail.com>
2025-04-11 15:29:56 +0200

    Injected SignInManager<ApplicationUser> into NewTube.Server.Services.AuthService; Added basic RequestLogin method to NewTube.Server.Services.AuthService;

--- NewTube/NewTube.Server/Services/AuthService.cs
+++ NewTube/NewTube.Server/Services/AuthService.cs
@@ -1,6 +1,26 @@
-﻿namespace NewTube.Server.Services
+﻿using Microsoft.AspNetCore.Identity;
+using NewTube.Server.Data;
+using NewTube.Shared.DataTransfer;
+
+namespace NewTube.Server.Services
 {
     public class AuthService
     {
+        private readonly SignInManager<ApplicationUser> SignInManager;
+
+        public AuthService(SignInManager<ApplicationUser> signInManager)
+        {
+            SignInManager = signInManager;
+        }
+
+        public void RequestLogin(LoginRequest loginRequest)
+        {
+            SignInManager.PasswordSignInAsync(
+                loginRequest.Username,
+                loginRequest.Password,
+                isPersistent: false,
+                lockoutOnFailure: false
+            ).Wait();
+        }
     }
 }
