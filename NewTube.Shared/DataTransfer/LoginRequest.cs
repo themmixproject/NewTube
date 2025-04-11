a74ba9b99e94c93ebb70caad210d7895fc915416
themmixproject <steven.devalk@gmail.com>
2025-04-11 20:27:19 +0200

    LoginRequest.Username and LoginRequest.Password have default empty string ("") values when initialized instead of null. This is because SignInManager.PasswordSignInAsync expects non-null values for username and password;

--- NewTube.Shared/DataTransfer/LoginRequest.cs
+++ NewTube.Shared/DataTransfer/LoginRequest.cs
@@ -6,6 +6,6 @@ public class LoginRequest
 {
     public LoginRequest() { }
 
-    public string Username { get; set;}
-    public string Password { get; set; }
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
 }
