f7346a5f2598d4d8ab9997509d7f6c94fbe437a9
themmixproject <steven.devalk@gmail.com>
2025-04-11 21:54:32 +0200

    Renamed _baseUrl property to _endPoint in NewTube.Client.Clients.AuthClient; Injected IAuthService into SignUpForm; Implemented RequestSignUpAsync in SignUpForm.requestSignUp method;

--- NewTube/NewTube.Client/Clients/AuthClient.cs
+++ NewTube/NewTube.Client/Clients/AuthClient.cs
@@ -7,7 +7,7 @@ namespace NewTube.Client.Clients
 {
     public class AuthClient : IAuthService
     {
-        private readonly string _baseUrl = "auth/";
+        private readonly string _endPoint = "auth";
         private readonly HttpClient _httpClient;
         
         public AuthClient(HttpClient httpClient)
@@ -17,14 +17,14 @@ namespace NewTube.Client.Clients
 
         public async Task<LoginResponse> RequestLoginAsync(LoginRequest loginRequest)
         {
-            var result = await _httpClient.PostAsJsonAsync($"{_baseUrl}login", loginRequest);
+            var result = await _httpClient.PostAsJsonAsync($"{_endPoint}/login", loginRequest);
             var contentString = await result.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<LoginResponse>(contentString)!;
         }
 
         public async Task<SignUpResponse> RequestSignUpAsync(SignUpRequest signUpRequest)
         {
-            var result = await _httpClient.PostAsJsonAsync($"{_baseUrl}/signup", signUpRequest);
+            var result = await _httpClient.PostAsJsonAsync($"{_endPoint}/signup", signUpRequest);
             var contentString = await result.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<SignUpResponse>(contentString)!;
         } 
