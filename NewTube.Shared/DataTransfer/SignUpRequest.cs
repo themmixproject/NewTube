bc1bf7a27ba6e4f7a8f9a9c549dbdaf8c7be5985
themmixproject <steven.devalk@gmail.com>
2025-04-11 20:49:11 +0200

    Added Email, Username and Password properties to NewTube.Shared.DataTransfer.SignUpRequest;

--- NewTube.Shared/DataTransfer/SignUpRequest.cs
+++ NewTube.Shared/DataTransfer/SignUpRequest.cs
@@ -8,5 +8,8 @@ namespace NewTube.Shared.DataTransfer
 {
     public class SignUpRequest
     {
+        public string Email { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
     }
 }
