f21026cc2d8fce545398bd922c7a325429d08ea9
themmixproject <steven.devalk@gmail.com>
2025-04-11 21:41:28 +0200

    Injected IUserStore<ApplicationUser> and UserManager<ApplicationUser> in NewTube.Server.Services.AuthService; Added explicit typing to result variable in NewTube.Server.Services.AuthService.ReuqestLoginAsync method; Added RequestSignUpAsync and GetEmailStore methods to NewTube.Server.Services.AuthService;

--- NewTube/NewTube.Server/Services/AuthService.cs
+++ NewTube/NewTube.Server/Services/AuthService.cs
@@ -8,15 +8,23 @@ namespace NewTube.Server.Services
     public class AuthService : IAuthService
     {
         private readonly SignInManager<ApplicationUser> SignInManager;
+        private readonly IUserStore<ApplicationUser> UserStore;
+        private readonly UserManager<ApplicationUser> UserManager;
 
-        public AuthService(SignInManager<ApplicationUser> signInManager)
+        public AuthService(
+            SignInManager<ApplicationUser> signInManager,
+            IUserStore<ApplicationUser> userStore,
+            UserManager<ApplicationUser> userManager
+            )
         {
             SignInManager = signInManager;
+            UserStore = userStore;
+            UserManager = userManager;
         }
 
         public async Task<LoginResponse> RequestLoginAsync(LoginRequest loginRequest)
         {
-            var result = await SignInManager.PasswordSignInAsync(
+            SignInResult result = await SignInManager.PasswordSignInAsync(
                 loginRequest.Username,
                 loginRequest.Password,
                 isPersistent: false,
@@ -29,6 +37,34 @@ namespace NewTube.Server.Services
             }
 
             return response;
+        }
+
+        public async Task<SignUpResponse> RequestSignUpAsync(SignUpRequest request)
+        {
+            ApplicationUser user = new ApplicationUser();
+            await UserStore.SetUserNameAsync(user, request.Email, CancellationToken.None);
+
+            IUserEmailStore<ApplicationUser> emailStore = GetEmailStore();
+            await emailStore.SetEmailAsync(user, request.Email, CancellationToken.None);
+
+            IdentityResult result = await UserManager.CreateAsync(user, request.Password);
+
+            SignUpResponse response = new SignUpResponse();
+            if (result.Succeeded)
+            {
+                response.IsSuccessful = true;
+            }
+
+            return response;
+        }
+
+        private IUserEmailStore<ApplicationUser> GetEmailStore()
+        {
+            if (!UserManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<ApplicationUser>)UserStore;
         }
     }
 }
