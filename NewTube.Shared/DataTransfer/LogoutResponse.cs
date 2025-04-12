b996c53810a702762138997a2d9ec4314796aa31
themmixproject <steven.devalk@gmail.com>
2025-04-12 12:15:46 +0200

    Added LogoutResponse class to NewTube.Shared.DataTransfer;

---
+++ NewTube.Shared/DataTransfer/LogoutResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTube.Shared.DataTransfer
+{
+    public class LogoutResponse
+    {
+        public bool IsSuccessful { get; set; }
+    }
+}
