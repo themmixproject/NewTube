f8445e37fb1d7ad3d1a39b56f9c7e7677dff0f0f
themmixproject <steven.devalk@gmail.com>
2025-04-16 19:03:46 +0200

    Added base for ClientAuthStateProvider in NewTube.Client;

---
+++ NewTube/NewTube.Client/ClientAuthStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace NewTube.Client
+{
+    public class ClientAuthStateProvider : AuthenticationStateProvider
+    {
+        public Task<AuthenticationState> GetAuthenticationStateAsync(string username)
+        {
+            var identity = new ClaimsIdentity([
+                new Claim(ClaimTypes.Name, username)    
+            ], "Custom Authentication");
+
+            var user = new ClaimsPrincipal(identity);
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
