206b1441b81a20167a91d704d3faa6af905240d0
themmixproject <steven.devalk@gmail.com>
2025-04-17 21:21:39 +0200

    Added constructor service injection for HttpClient and AuthClient in NewTube.Client.ClientAuthStateProvider; Added missing semicolon (;) on System.Security.Claims using statement in NewTube.Client.ClientAuthStateProvider; Added NewTube.Client.Clients using statement in NewTube.Client.ClientAuthStateProvider;

--- NewTube/NewTube.Client/ClientAuthStateProvider.cs
+++ NewTube/NewTube.Client/ClientAuthStateProvider.cs
@@ -1,11 +1,19 @@
-﻿using System.Security.Claims
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
+using NewTube.Client.Clients;
 
 namespace NewTube.Client
 {
     public class ClientAuthStateProvider : AuthenticationStateProvider
     {
-        public Task<AuthenticationState> GetAuthenticationStateAsync(string username)
+        private readonly AuthClient AuthClient;
+        private readonly HttpClient HttpClient;
+        public ClientAuthStateProvider(AuthClient authClient, HttpClient httpClient)
+        {
+            this.AuthClient = authClient;
+            this.HttpClient = httpClient;
+        }
+
         {
             var identity = new ClaimsIdentity([
                 new Claim(ClaimTypes.Name, username)    
