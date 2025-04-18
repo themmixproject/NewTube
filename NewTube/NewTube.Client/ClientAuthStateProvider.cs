c3ed852a1b96bb03e9d85c30fcc0c25035d228a1
themmixproject <steven.devalk@gmail.com>
2025-04-18 11:37:45 +0200

    Added JsonSerializerOptions property to ClientAuthStateProvider; Added GetAuthenticationStateAsync method to ClientAuthStateProvider;

--- NewTube/NewTube.Client/ClientAuthStateProvider.cs
+++ NewTube/NewTube.Client/ClientAuthStateProvider.cs
@@ -9,9 +9,18 @@ namespace NewTube.Client
 {
     public class ClientAuthStateProvider : AuthenticationStateProvider
     {
+        /// <summary>
+        /// Map the JavaScript-formatted properties to C#-formatted classes.
+        /// </summary>
+        private readonly JsonSerializerOptions JsonSerializerOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        
         private readonly AuthClient AuthClient;
         private readonly HttpClient HttpClient;
         private readonly ILogger Logger;
+
         public ClientAuthStateProvider(AuthClient authClient, HttpClient httpClient, ILogger<ClientAuthStateProvider> logger)
         {
             this.AuthClient = authClient;
@@ -122,6 +131,89 @@ namespace NewTube.Client
                 Succeeded = false,
                 ErrorList = ["Invalid email and/or password."]
             };
+        }
+
+        /// <summary>
+        /// Get authentication state.
+        /// </summary>
+        /// <remarks>
+        /// Called by Blazor anytime and authentication-based decision needs to
+        /// be made, then cached until the changed state notification is raised.
+        /// </remarks>
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            IsAuthenticated = false;
+
+            // default to not authenticated
+            var user = Unauthenticated;
+
+            try
+            {
+                // the user info endpoint is secured, so if the user isn't loggedin this will fail
+                var userResponse = await HttpClient.GetAsync("manage/info");
+
+                // throw if user info wasn't retrieved
+                userResponse.EnsureSuccessStatusCode();
+
+                // user is authenticated, so let's build their authenticated identity
+                var userJson = await userResponse.Content.ReadAsStringAsync();
+                var userInfo = JsonSerializer.Deserialize<UserInfo>(userJson, JsonSerializerOptions);
+
+                if (userInfo != null)
+                {
+                    // in this example app, name and email are the same
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, userInfo.Email),
+                        new Claim(ClaimTypes.Email, userInfo.Email)
+                    };
+
+                    // add any additional claims
+                    claims.AddRange(
+                        userInfo.Claims.Where(
+                            c => c.Key != ClaimTypes.Name && !string.IsNullOrEmpty(c.Value)
+                        ).Select(
+                            c => new Claim(c.Key, c.Value)
+                        )
+                    );
+
+                    // request the roles endpoint for the user's roles
+                    var rolesResponse = await HttpClient.GetAsync("roles");
+
+                    // throw if request fails
+                    rolesResponse.EnsureSuccessStatusCode();
+
+                    // read th response into a string
+                    var rolesJson = await rolesResponse.Content.ReadAsStringAsync();
+
+                    // deserialize the roles string into an array
+                    var roles = JsonSerializer.Deserialize<RoleClaim[]>(rolesJson, JsonSerializerOptions);
+
+                    // add any roles to the claims collection
+                    if (roles?.Length > 0)
+                    {
+                        foreach (var role in roles)
+                        {
+                            if (!string.IsNullOrEmpty(role.Type) && !string.IsNullOrEmpty(role.Value))
+                            {
+                                claims.Add(new Claim(role.Type, role.Value, role.ValueType, role.Issuer, role.OriginalIssuer));
+                            }
+                        }
+                    }
+
+                    // set the principal
+                    var id = new ClaimsIdentity(claims, nameof(ClientAuthStateProvider));
+                    user = new ClaimsPrincipal(id);
+                    IsAuthenticated = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "App error");
+            }
+
+            // return the state
+            return new AuthenticationState(user);
         }
     }
 }
