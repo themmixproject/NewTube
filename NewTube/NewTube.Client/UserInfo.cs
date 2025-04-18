13a6cbed26614308635492c6dc9784b9ba7280f6
themmixproject <steven.devalk@gmail.com>
2025-04-18 10:38:07 +0200

    Added Name, UserIdClaimType, NameClaimType to NewTube.Client.UserInfo class; Added FromClaimsPrincipal, ToClaimsPrincipal and GetRequiredClaim methods to NewTube.Client.UserInfo;

--- NewTube/NewTube.Client/UserInfo.cs
+++ NewTube/NewTube.Client/UserInfo.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 namespace NewTube.Client
 {
     // Add properties to this class and update the server and client AuthenticationStateProviders
@@ -6,5 +8,40 @@ namespace NewTube.Client
     {
         public required string UserId { get; set; }
         public required string Email { get; set; }
+        public required string Name { get; set; }
+
+        public const string UserIdClaimType = "sub";
+        public const string NameClaimType = "name";
+
+        public static UserInfo FromClaimsPrincipal(ClaimsPrincipal principal)
+        {
+            return new UserInfo
+            {
+                UserId = GetRequiredClaim(principal, UserIdClaimType),
+                Name = GetRequiredClaim(principal, NameClaimType),
+                Email = ""
+            };
+        }
+
+        public ClaimsPrincipal ToClaimsPrincipal()
+        {
+            return new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    [
+                        new Claim(UserIdClaimType, UserId),
+                        new Claim(NameClaimType, Name)
+                    ],
+                    authenticationType: nameof(UserInfo),
+                    nameType: NameClaimType,
+                    roleType: null
+                )
+            );
+        }
+
+        public static string GetRequiredClaim(ClaimsPrincipal principal, string claimType)
+        {
+            return principal.FindFirst(claimType)?.Value ?? throw new InvalidOperationException($"Could not find reuired '{claimType}' claim.");
+        }
+
     }
 }
