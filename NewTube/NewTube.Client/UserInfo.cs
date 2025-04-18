de30c65d0b67f10f2f214fba73964885fa8fb1fd
themmixproject <steven.devalk@gmail.com>
2025-04-18 10:50:48 +0200

    Replaced UserInfo code with UserInfo code from correct example reference (https://github.com/dotnet/blazor-samples/blob/main/9.0/BlazorWebAssemblyStandaloneWithIdentity/BlazorWasmAuth/Identity/Models/UserInfo.cs) because the wrong code was copied;

--- NewTube/NewTube.Client/UserInfo.cs
+++ NewTube/NewTube.Client/UserInfo.cs
@@ -1,47 +1,25 @@
-using System.Security.Claims;
-
 namespace NewTube.Client
 {
     // Add properties to this class and update the server and client AuthenticationStateProviders
     // to expose more information about the authenticated user to the client.
+
+    /// <summary>
+    /// User info from identity endpoint to establish claims.
+    /// </summary>
     public class UserInfo
     {
         public required string UserId { get; set; }
+
+        /// <summary>
+        /// The email address.
+        /// </summary>
         public required string Email { get; set; }
-        public required string Name { get; set; }
 
-        public const string UserIdClaimType = "sub";
-        public const string NameClaimType = "name";
+        public bool IsEmailConfigured { get; set; }
 
-        public static UserInfo FromClaimsPrincipal(ClaimsPrincipal principal)
-        {
-            return new UserInfo
-            {
-                UserId = GetRequiredClaim(principal, UserIdClaimType),
-                Name = GetRequiredClaim(principal, NameClaimType),
-                Email = ""
-            };
-        }
-
-        public ClaimsPrincipal ToClaimsPrincipal()
-        {
-            return new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    [
-                        new Claim(UserIdClaimType, UserId),
-                        new Claim(NameClaimType, Name)
-                    ],
-                    authenticationType: nameof(UserInfo),
-                    nameType: NameClaimType,
-                    roleType: null
-                )
-            );
-        }
-
-        public static string GetRequiredClaim(ClaimsPrincipal principal, string claimType)
-        {
-            return principal.FindFirst(claimType)?.Value ?? throw new InvalidOperationException($"Could not find reuired '{claimType}' claim.");
-        }
-
+        /// <summary>
+        /// The list of claims for the user.
+        /// </summary>
+        public Dictionary<string, string> Claims { get; set; } = [];
     }
 }
