8e11801989962caad08cc8a0de2643879febf2da
themmixproject <steven.devalk@gmail.com>
2025-04-19 21:23:34 +0200

    Added new /login route derived from Microsoft.AspNetCore.Routing.IdentityApiEndpointRouteBuilderExtensions to AuthController;

--- NewTube/NewTube.Server/Controllers/AuthController.cs
+++ NewTube/NewTube.Server/Controllers/AuthController.cs
@@ -4,20 +4,49 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using NewTube.Shared.DataTransfer;
 using NewTube.Shared.Interfaces;
 using NewTube.Server.Services;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Identity;
+using NewTube.Server.Data;
 
 namespace NewTube.Server.Controllers
 {
     [Route("[controller]")]
     public class AuthController
     {
-        public AuthController() { }
+        private readonly SignInManager<ApplicationUser> SignInManager;
 
-        [HttpPost("login")]
-        public IResult LoginUser([FromBody] LoginRequest loginRequest)
+        public AuthController(SignInManager<ApplicationUser> signInManager)
         {
-            return Results.Ok("ok");
+            SignInManager = signInManager;
         }
 
+        [HttpPost("login")]
+        public async Task<Results
+            <Ok<AccessTokenResponse>, EmptyHttpResult, ProblemHttpResult>> LoginUser
+            (
+                [FromBody] LoginRequest login,
+                [FromQuery] bool? useCookies,
+                [FromQuery] bool? useSessionCookies
+            )
+        {
+            var useCookieScheme = (useCookies == true) || (useSessionCookies == true);
+            var isPersistent = (useCookies == true) && (useSessionCookies != true);
+            SignInManager.AuthenticationScheme = useCookieScheme ? IdentityConstants.ApplicationScheme : IdentityConstants.BearerScheme;
+
+            var result = await SignInManager.PasswordSignInAsync(
+                login.Username,
+                login.Password,
+                isPersistent,
+                lockoutOnFailure: true
+            );
+
+            if (!result.Succeeded)
+            {
+                return TypedResults.Problem(result.ToString(), statusCode: StatusCodes.Status401Unauthorized);
+            }
+
+            return TypedResults.Empty;
+        }
         //[HttpPost("login")]
         //public async Task<LoginResponse> LoginUser([FromBody] LoginRequest loginRequest)
         //{
