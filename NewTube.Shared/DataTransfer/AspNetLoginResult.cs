0d1aba0e97c204409df62bd429c0532ef5c74591
themmixproject <steven.devalk@gmail.com>
2025-04-25 11:09:18 +0200

    Added AspNetLoginResult class t handle default respone of /auth/login endpoint;

---
+++ NewTube.Shared/DataTransfer/AspNetLoginResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTube.Shared.DataTransfer
+{
+    public class AspNetLoginResult
+    {
+        public string Token { get; set; }
+        public DateTime Expiry { get; set; }
+    }
+}
