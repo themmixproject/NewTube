6ba6416c1582cc628625172575a7611302a9844f
themmixproject <steven.devalk@gmail.com>
2025-05-04 13:45:57 +0200

    Set RequestResponse to be response of Request methods in IAuthService;

--- NewTube.Shared/Interfaces/IAuthService.cs
+++ NewTube.Shared/Interfaces/IAuthService.cs
@@ -4,9 +4,9 @@ namespace NewTube.Shared.Interfaces
 {
     public interface IAuthService
     {
-        public Task RequestLoginAsync(LoginRequest loginRequest);
-        public Task RequestLogoutAsync();
-        public Task RequestSignUpAsync(SignUpRequest signUpRequest);
+        public Task<RequestResponse> RequestLoginAsync(LoginRequest loginRequest);
+        public Task<RequestResponse> RequestLogoutAsync();
+        public Task<RequestResponse> RequestSignUpAsync(SignUpRequest signUpRequest);
         public Task<bool> CheckIfAuthenticatedAsync();
     }
 }
