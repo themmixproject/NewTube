4baa5f7a3b39da649b0f08fde66fbc584c262d55
themmixproject <steven.devalk@gmail.com>
2025-05-04 17:24:14 +0200

    Commented out AuthService implementation;

--- NewTube/NewTube.Server/Services/AuthService.cs
+++ NewTube/NewTube.Server/Services/AuthService.cs
@@ -1,77 +1,77 @@
-﻿using Microsoft.AspNetCore.Identity;
-using NewTube.Server.Data;
-using NewTube.Shared.DataTransfer;
-using NewTube.Shared.Interfaces;
+﻿//using Microsoft.AspNetCore.Identity;
+//using NewTube.Server.Data;
+//using NewTube.Shared.DataTransfer;
+//using NewTube.Shared.Interfaces;
 
-namespace NewTube.Server.Services
-{
-    public class AuthService : IAuthService
-    {
-        private readonly SignInManager<ApplicationUser> SignInManager;
-        private readonly IUserStore<ApplicationUser> UserStore;
-        private readonly UserManager<ApplicationUser> UserManager;
+//namespace NewTube.Server.Services
+//{
+//    public class AuthService : IAuthService
+//    {
+//        private readonly SignInManager<ApplicationUser> SignInManager;
+//        private readonly IUserStore<ApplicationUser> UserStore;
+//        private readonly UserManager<ApplicationUser> UserManager;
 
-        public AuthService(
-            SignInManager<ApplicationUser> signInManager,
-            IUserStore<ApplicationUser> userStore,
-            UserManager<ApplicationUser> userManager
-            )
-        {
-            SignInManager = signInManager;
-            UserStore = userStore;
-            UserManager = userManager;
-        }
+//        public AuthService(
+//            SignInManager<ApplicationUser> signInManager,
+//            IUserStore<ApplicationUser> userStore,
+//            UserManager<ApplicationUser> userManager
+//            )
+//        {
+//            SignInManager = signInManager;
+//            UserStore = userStore;
+//            UserManager = userManager;
+//        }
 
-        public async Task RequestLoginAsync(LoginRequest loginRequest)
-        {
-            SignInResult result = await SignInManager.PasswordSignInAsync(
-                loginRequest.Username,
-                loginRequest.Password,
-                isPersistent: false,
-                lockoutOnFailure: false
-            );
+//        public async Task RequestLoginAsync(LoginRequest loginRequest)
+//        {
+//            SignInResult result = await SignInManager.PasswordSignInAsync(
+//                loginRequest.Username,
+//                loginRequest.Password,
+//                isPersistent: false,
+//                lockoutOnFailure: false
+//            );
 
-            //LoginResponse response = new LoginResponse();
-            //if (result.Succeeded)
-            //{
-            //    response.IsSuccessful = true;
-            //}
+//            //LoginResponse response = new LoginResponse();
+//            //if (result.Succeeded)
+//            //{
+//            //    response.IsSuccessful = true;
+//            //}
 
-            //return response;
-        }
+//            //return response;
+//        }
 
-        public async Task RequestLogoutAsync()
-        {
-            await SignInManager.SignOutAsync();
-            //return new LogoutResponse { IsSuccessful = true };
-        }
+//        public async Task RequestLogoutAsync()
+//        {
+//            await SignInManager.SignOutAsync();
+//            //return new LogoutResponse { IsSuccessful = true };
+//        }
 
-        public async Task RequestSignUpAsync(SignUpRequest request)
-        {
-            ApplicationUser user = new ApplicationUser();
-            await UserStore.SetUserNameAsync(user, request.Username, CancellationToken.None);
+//        public async Task RequestSignUpAsync(SignUpRequest request)
+//        {
+//            ApplicationUser user = new ApplicationUser();
+//            await UserStore.SetUserNameAsync(user, request.Username, CancellationToken.None);
 
-            IUserEmailStore<ApplicationUser> emailStore = GetEmailStore();
-            await emailStore.SetEmailAsync(user, request.Email, CancellationToken.None);
+//            IUserEmailStore<ApplicationUser> emailStore = GetEmailStore();
+//            await emailStore.SetEmailAsync(user, request.Email, CancellationToken.None);
 
-            IdentityResult result = await UserManager.CreateAsync(user, request.Password);
+//            IdentityResult result = await UserManager.CreateAsync(user, request.Password);
 
-            //SignUpResponse response = new SignUpResponse();
-            //if (result.Succeeded)
-            //{
-            //    response.IsSuccessful = true;
-            //}
+//            //SignUpResponse response = new SignUpResponse();
+//            //if (result.Succeeded)
+//            //{
+//            //    response.IsSuccessful = true;
+//            //}
 
-            //return response;
-        }
+//            //return response;
+//        }
 
-        private IUserEmailStore<ApplicationUser> GetEmailStore()
-        {
-            if (!UserManager.SupportsUserEmail)
-            {
-                throw new NotSupportedException("The default UI requires a user store with email support.");
-            }
-            return (IUserEmailStore<ApplicationUser>)UserStore;
-        }
-    }
-}
+//        private IUserEmailStore<ApplicationUser> GetEmailStore()
+//        {
+//            if (!UserManager.SupportsUserEmail)
+//            {
+//                throw new NotSupportedException("The default UI requires a user store with email support.");
+//            }
+//            return (IUserEmailStore<ApplicationUser>)UserStore;
+//        }
+//    }
+//}
