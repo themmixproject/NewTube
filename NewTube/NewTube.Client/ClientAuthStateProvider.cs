bd626d44988eb5349ead80ee4828788130ffb389
themmixproject <steven.devalk@gmail.com>
2025-05-16 21:43:38 +0200

    Added new implementation for ClientAuthStateProvider;

--- NewTube/NewTube.Client/ClientAuthStateProvider.cs
+++ NewTube/NewTube.Client/ClientAuthStateProvider.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace NewTube.Client
 {
-    public internal class ClientAuthenticationStateProvider : AuthenticationStateProvider
+    internal class ClientAuthenticationStateProvider : AuthenticationStateProvider
     {
+        private readonly HttpClient HttpClient;
+
+        public ClientAuthenticationStateProvider(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            var response = await HttpClient.GetAsync("/_auth/me");
+            if (response == null) { return new AuthenticationState(new ClaimsPrincipal()); }
 
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var principal = JsonSerializer.Deserialize<ClaimsPrincipal>(responseContent)!;
+
+            return new AuthenticationState(principal);
         }
     }
 }
