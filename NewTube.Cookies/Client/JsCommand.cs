f9fc50aa6ceb9e897ed2957fd44f67a61518d6d6
themmixproject <steven.devalk@gmail.com>
2025-05-21 15:30:08 +0200

    Added JsCommand class with SetCookie method;

---
+++ NewTube.Cookies/Client/JsCommand.cs
@@ -0,0 +1,26 @@
+﻿using NewTube.Cookies.Models;
+using System.Text;
+
+namespace NewTube.Cookies.Client
+{
+    internal class JsCommand
+    {
+        public static string SetCookie(Cookie cookie)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append($"document.cookie = \"");
+            stringBuilder.Append($"{cookie.Key}={cookie.Value};");
+            stringBuilder.Append($"expires={cookie.Expiration:R};");
+
+            stringBuilder.Append($"path=/");
+            if (cookie.SameSiteMode.HasValue)
+            {
+                stringBuilder.Append(";");
+                stringBuilder.Append($"SameSite={cookie.SameSiteMode.Value.ToString()}");
+            }
+                
+            stringBuilder.Append("\"");
+            return stringBuilder.ToString();
+        }
+    }
+}
