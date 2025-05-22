9b14805ca9300c894a0433acff5d9c4ae2070314
themmixproject <steven.devalk@gmail.com>
2025-05-22 10:37:19 +0200

    Added IServiceCollectionExtensions class with AddCookieService method to make initializing cookie-service easier for user; Installed Microsoft.AspNetCore.Components, Microsoft.Components.WebAssembly and Microsoft.Extensions.DependencyInjection packages in NewTube.Cookies project;

---
+++ NewTube.Cookies/Client/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NewTube.Cookies.Client.Services;
+using NewTube.Cookies.Interfaces;
+
+namespace NewTube.Cookies.Client.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCookieService(this IServiceCollection services)
+        {
+            services.AddScoped<ICookieService, JsInteropCookiesService>();
+            
+            return services;
+        }
+    }
+}
