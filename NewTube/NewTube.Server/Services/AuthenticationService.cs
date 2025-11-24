6e976e1daf1ab33bd4edce6a8df14822c7556d26
themmixproject <steven.devalk@gmail.com>
2025-11-24 20:31:03 +0100

    Added base for AuthenticationService implementation in NewTube.Server

---
+++ NewTube/NewTube.Server/Services/AuthenticationService.cs
@@ -0,0 +1,9 @@
+using NewTube.Shared.Interfaces;
+
+namespace NewTube.Server.Services;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public void LoginUser() {}
+    public void RegisterUser() {}
+}
