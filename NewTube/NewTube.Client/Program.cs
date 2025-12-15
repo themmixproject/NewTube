3ba128bf087d18b55edd0b5dac0eb7ebfd78c782
themmixproject <steven.devalk@gmail.com>
2025-12-15 18:28:52 +0100

    Added cookieService and HttpClient scoped services in client

--- NewTube/NewTube.Client/Program.cs
+++ NewTube/NewTube.Client/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using NewTube.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NewTube.Client.Services;
 using NewTube.Shared.Interfaces;
+using MMIX.Blazor.Cookies.Client;
 
 namespace NewTube.Client
 {
@@ -11,6 +13,15 @@ namespace NewTube.Client
         static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddScoped<HttpClient>(serviceProvider =>
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                return client;
+            });
+
+            builder.Services.AddCookieService();
 
             builder.Services.AddAuthorizationCore();
             builder.Services.AddCascadingAuthenticationState();
